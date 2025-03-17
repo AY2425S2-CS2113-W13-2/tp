4bb4d477f8df763a032b1b0cef115dea4322e49c
YanXinyaoo <e1155531@u.nus.edu>
2025-03-17 22:36:38 +0800

    Modify according to code standard

--- src/test/java/seedu/event/EventManagerTest.java
+++ src/test/java/seedu/event/EventManagerTest.java
@@ -1,6 +1,7 @@
 package seedu.event;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ui.UI;
@@ -13,12 +14,13 @@ import java.util.ArrayList;
 class EventManagerTest {
     private EventManager eventManager;
     private UI mockUI;
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+    DateTimeFormatter formatter;
 
     @BeforeEach
     void setUp() {
         mockUI = new UI();
         eventManager = new EventManager(new ArrayList<>(), mockUI);
+        formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
     }
 
     @Test
@@ -34,7 +36,8 @@ class EventManagerTest {
     @Test
     void testGetEventValidIndex() throws SyncException {
         Event event = new Event("Team Meeting", LocalDateTime.parse("2025/05/10 14:00", formatter),
-                LocalDateTime.parse("2025/05/10 16:00", formatter), "Conference Room", "Team Meeting");        eventManager.addEvent(event);
+            LocalDateTime.parse("2025/05/10 16:00", formatter), "Conference Room", "Team Meeting");
+        eventManager.addEvent(event);
 
         Event fetchedEvent = eventManager.getEvent(0);
         assertEquals(event, fetchedEvent);
@@ -50,9 +53,9 @@ class EventManagerTest {
     @Test
     void testSize() {
         Event event1 = new Event("Team Meeting", LocalDateTime.parse("2025/05/10 14:00", formatter),
-                LocalDateTime.parse("2025/05/10 16:00", formatter), "Conference Room", "Team Meeting");
+            LocalDateTime.parse("2025/05/10 16:00", formatter), "Conference Room", "Team Meeting");
         Event event2 = new Event("Team Meeting2", LocalDateTime.parse("2025/05/11 14:00", formatter),
-                LocalDateTime.parse("2025/05/11 16:00", formatter), "Conference Room", "Team Meeting");
+            LocalDateTime.parse("2025/05/11 16:00", formatter), "Conference Room", "Team Meeting");
         eventManager.addEvent(event1);
         eventManager.addEvent(event2);
 
