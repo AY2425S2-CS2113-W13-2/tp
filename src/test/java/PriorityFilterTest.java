d6d174d93d491b7c266c11f712f0220fb2b9bbdd
yeeern27 <141848485+yeeern27@users.noreply.github.com>
2025-03-27 05:02:01 +0800

    Revert "Coding Style Edited"

--- src/test/java/PriorityFilterTest.java
+++ src/test/java/PriorityFilterTest.java
@@ -1,7 +1,6 @@
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import event.Event;
 import event.EventManager;
 import label.Priority;
