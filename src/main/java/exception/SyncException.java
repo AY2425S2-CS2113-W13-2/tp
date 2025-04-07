4e2fc417ac609f337e008c6009b2406d0db48ce7
YanXinyaoo <e1155531@u.nus.edu>
2025-04-07 10:23:57 +0800

    update notification when error occurs to let users type command word again

--- src/main/java/exception/SyncException.java
+++ src/main/java/exception/SyncException.java
@@ -21,35 +21,23 @@ public class SyncException extends Exception {
     }
 
     public static String invalidEventIndexErrorMessage() {
-        return "   (╯°□°)╯︵ Invalid event index!\n" +
+        return "   (╯°□°)╯︵ Invalid event index! Enter your command word to try again.\n" +
                 "   Please provide a valid event index.\n" +
                 "   Example: `delete 2` or `edit 3`.";
     }
 
     public static String invalidEventDetailsErrorMessage() {
-        return "   (╯°□°)╯︵ OOPS!!! Invalid event details!\n" +
+        return "   (╯°□°)╯︵ OOPS!!! Invalid event details! Enter 'add' to try again.\n" +
                 "   Please provide valid event details in the format: \n" +
                 "   `add Event Name | Start Date | End Date | Location | Description`.\n" +
                 "   Example: `add Meeting | 2025/05/10 14:00 | 2025/05/10 15:00 | Room 101 | Discuss project`.";
     }
 
     public static String invalidFilterInputErrorMessage() {
-        return "   (╯°□°)╯︵ Invalid filter input!\n" +
+        return "   (╯°□°)╯︵ Invalid filter input! Enter 'filter' to try again.\n" +
                 "   Please provide valid filter inputs in the format: \n" +
                 "   `filter {lower priority bound} {upper priority bound}`.\n" +
                 "   Example: `filter 1 2`.";
-    }
-
-    public static String eventNotFoundErrorMessage() {
-        return "   (╯°□°)╯︵ Event not found!\n" +
-                "   The event you are trying to delete or edit does not exist.\n" +
-                "   Please ensure you are using the correct event index.";
-    }
-
-    public static String invalidDuplicateEventIndexErrorMessage() {
-        return "   (╯°□°)╯︵ Invalid event index for duplication!\n" +
-                "   Please provide a valid event index to duplicate.\n" +
-                "   Example: `duplicate 2 New Event Name`.";
     }
 
     public static String invalidBoundErrorMessage() {
